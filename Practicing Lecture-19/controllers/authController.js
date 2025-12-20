ba34d29e05493e7d512af32997ccd9236ef139cd
Fazo01 <fg7829098@gmail.com>
2025-12-20 11:46:25 +0500

    SignUp Ui in practicing lecture 19

--- Practicing Lecture-19/controllers/authController.js
+++ Practicing Lecture-19/controllers/authController.js
@@ -1,12 +1,17 @@
 exports.getLogin=(req,res,next)=>{
     res.render("auth/login", {pageTitle: "Login", currentPage: "addhome",isLoggedIn:false})
-
+}
+exports.getSignup=(req,res,next)=>{
+    res.render("auth/signup", {pageTitle: "SignUp", currentPage: "signup",isLoggedIn:false})
+}
+exports.postSignup=(req,res,next)=>{
+    console.log(req.body)
+    // req.session.isLoggedIn=true
+    res.redirect("/")
 }
 exports.postLogin=(req,res,next)=>{
-    // console.log(req.body)
+    console.log(req.body)
     req.session.isLoggedIn=true
-    // res.session("isLoggedIn",true)
-    // res.isLoggedIn=true
     res.redirect("/")
 }
 exports.postLogout=(req,res,next)=>{
